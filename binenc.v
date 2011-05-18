41e85f07f1749d22f72fba71b811ba7e7203afce
minad <mail@daniel-mendler.de>
2011-05-18 13:48:19 +0200

    binenc fixed

--- binenc.v
+++ binenc.v
@@ -7,18 +7,18 @@ module binenc
    output [$clog2(n)-1:0] out);
 
    wire [$clog2(n)-1:0] o[n:0];
-   wire [n:0]      	h;
+   wire [n:0]      	found;
 
    assign o[n] = 0;
-   assign h[n] = 0;
+   assign found[n] = 0;
    assign out = o[0];
 
    generate
       genvar i;
       for (i = n - 1; i >= 0; i = i - 1)
 	begin
-	   assign h[i] = in[i] & ~h[i + 1];
-	   assign o[i] = o[i + 1] | (h[i] ? i : 0);
+	   assign found[i] = found[i+1] | in[i];
+	   assign o[i] = o[i+1] | (in[i] & ~found[i+1] ? i : 0);
 	end
    endgenerate
 endmodule
