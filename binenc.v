25f5405b54dc1254ecbd4212ac90704996e43871
minad <mail@daniel-mendler.de>
2011-05-18 13:26:05 +0200

    use one loop

--- binenc.v
+++ binenc.v
@@ -5,17 +5,20 @@ module binenc
   #(parameter n = `DEFAULT_WIDTH)
   (input [n-1:0]          in,
    output [$clog2(n)-1:0] out);
-   
+
    wire [$clog2(n)-1:0] o[n:0];
-   wire [n-1:0] in_hot_one;
-   assign o[0] = 0;
-   assign out = o[n];
-   assign in_hot_one[n-1] = in[n-1];
+   wire [n:0]      	h;
+
+   assign o[n] = 0;
+   assign h[n] = 0;
+   assign out = o[0];
+
    generate
       genvar i;
-      for (i = 0; i < n; i = i + 1)
-	assign o[i+1] = o[i] | (in_hot_one & (1 << i) ? i : 0);
-      for (i=0; i<n-1; i=i+1)
-	assign in_hot_one[i] = in[i]&(~in_hot_one[i+1]);
+      for (i = n - 1; i >= 0; i = i - 1)
+	begin
+	   assign h[i] = in[i] & ~h[i + 1];
+	   assign o[i] = o[i + 1] | (h[i] ? i : 0);
+	end
    endgenerate
 endmodule
