0478a81f8c1059a1381f9de2e4c4466040842576
minad <mail@daniel-mendler.de>
2011-05-18 03:46:25 +0200

    learning verilog...

--- shifter_tb.v
+++ shifter_tb.v
@@ -9,20 +9,17 @@ module shifter_tb;
    shifter #(8) lsl(.a(a), .b(b), .out(lsl_out), .rot(1'b0), .left(1'b1), .sign(1'b0));
    shifter #(8) lsr(.a(a), .b(b), .out(lsr_out), .rot(1'b0), .left(1'b0), .sign(1'b1));
 
+   integer i;
+
    initial begin
       $display("Shifter Test Bench");
       $monitor("%b rol %d = %b | %b ror %d = %b | %b << %d = %b | %b >> %d = %b",
 	       a, b, rol_out, a, b, ror_out, a, b, lsl_out, a, b, lsr_out);
-   end
 
-   genvar i;
-   generate
       for (i = 0; i < 8; i = i + 1) begin
-	 initial begin
-	    #i
-	    a = 8'b10000111;
-	    b = i;
-	 end
+	 a = 8'b10000111;
+	 b = i;
+	 #1;
       end
-   endgenerate
+   end
 endmodule
